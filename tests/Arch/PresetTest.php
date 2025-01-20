8e09e47c4b7b7318de97d864d14b69e309730031
Nuno Maduro <enunomaduro@gmail.com>
2025-01-20 21:48:37 +0000

    chore(arch-testing): merges existing arch testing

--- tests/Arch/PresetTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-arch()->preset()->php();
-arch()->preset()->laravel();
-arch()->preset()->security();
--- tests/Unit/ArchTest.php
+++ tests/Unit/ArchTest.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+arch()->preset()->php();
+arch()->preset()->laravel();
+arch()->preset()->security();
+
+arch('controllers')
+    ->expect('App\Http\Controllers')
+    ->toExtendNothing()
+    ->not->toBeUsed();
+
 arch('exceptions')
     ->expect('App\Exceptions')
     ->toImplement('Throwable')
@@ -9,4 +18,4 @@ arch('exceptions')
         'App\Console\Commands',
         'App\Http\Controllers',
         'App\Services',
-]);
+    ]);
