4b204396f27889d5c9e6f0f9045ac58f473b78a3
Emir Kurtanovic <emir.kurtanovic@burda.com>
2025-01-21 10:17:32 +0100

    fix: expects controllers not to be used

--- tests/Unit/ArchTest.php
+++ tests/Unit/ArchTest.php
@@ -11,6 +11,10 @@ arch()->preset()->php();
 arch()->preset()->laravel();
 arch()->preset()->security();
 
+arch('controllers')
+    ->expect('App\Http\Controllers')
+    ->not->toBeUsed();
+
 arch('strict types')
     ->expect('App')
     ->toUseStrictTypes();
