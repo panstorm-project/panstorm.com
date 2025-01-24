972e58d2a288eb040ea1800e056aa82b136629d1
Nuno Maduro <enunomaduro@gmail.com>
2025-01-24 20:46:13 +0000

    chore: adds typescript boilerplate

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## PANStorm - privacy-focused product analytics
 
-PANStorm is a lightweight, privacy-first web-based product analytics application. It’s designed to track pages and components on your website with minimal data collection, 
+PANStorm is a lightweight, privacy-first web-based product analytics application. It’s designed to track pages and components on your website with minimal data collection,
 respecting user privacy while providing insights into your product usage.
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
-import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+    build: {
+        outDir: 'public',
+        rollupOptions: {
+            input: 'resources/js/monitor/app.ts',
+            output: {
+                entryFileNames: 'monitor.js',
+                format: 'iife',
+            },
+        },
+        manifest: false, // Disable the manifest file
+        assetsDir: '', // Disable the assets folder
+        emptyOutDir: false, // Prevent Vite from removing the contents of outDir
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.ts'],
             refresh: true,
         }),
     ],
