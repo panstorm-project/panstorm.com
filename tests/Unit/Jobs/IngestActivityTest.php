3ae193d34e6180242e72917ba4bf4a31a139dd12
Fábio Ferreira <fabio.ferreira@noop.pt>
2025-01-25 09:43:02 +0000

    test: Update view event test case with additional events

--- tests/Unit/Jobs/IngestActivityTest.php
+++ tests/Unit/Jobs/IngestActivityTest.php
@@ -18,6 +18,8 @@ it('can ingest activity events', function () {
             EventType::viewDuration('/about', 6),
             EventType::view('/about'),
             EventType::viewDuration('/about', 2),
+            EventType::view('/about'),
+            EventType::viewDuration('/about', 4),
         ],
     ]);
 
@@ -32,8 +34,8 @@ it('can ingest activity events', function () {
     expect($activity->fresh())->toBeNull()
         ->and(Page::count())->toBe(1)
         ->and($page->path)->toBe('about')
-        ->and($page->views)->toBe(2)
-        ->and($page->average_time)->toBe(7)
+        ->and($page->views)->toBe(3)
+        ->and($page->average_time)->toBe(6)
         ->and($page->bucket->format('Y-m-d H:i:s'))->toBe(now()->format('Y-m-d H:00:00'));
 });
 
